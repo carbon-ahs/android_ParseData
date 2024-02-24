4bc26b9cf15a1e31e922bd229adf6c02ef87a101
carbon-ahs <shehanuk.ahsan@gmail.com>
2024-02-24 13:40:42 +0600

    internet permission done, api fetching done

--- app/src/main/java/com/axiagroups/parsedata/MainActivity.java
+++ app/src/main/java/com/axiagroups/parsedata/MainActivity.java
@@ -3,12 +3,29 @@ package com.axiagroups.parsedata;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
+
+    String GOOGLE_URL = "https://www.google.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, GOOGLE_URL, response -> {
+            Log.d("Main", "onResponse:" + response.substring(0, 20));
+        }, error -> {
+            Log.d("Main", "onResponse: Failed to fetch API");
+        });
+        requestQueue.add(stringRequest);
     }
 }
